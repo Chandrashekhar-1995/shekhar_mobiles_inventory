d92961104d75108d5285763e632f440157e7bd51
Chandra Shekhar <jmbd.group@gmail.com>
2025-05-06 14:16:43 +0530

    .

--- src/controllers/fault.controllers.js
+++ src/controllers/fault.controllers.js
@@ -1,4 +1,3 @@
-import { Account } from "../models/account.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
